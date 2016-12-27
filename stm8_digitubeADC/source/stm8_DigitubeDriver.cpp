03413da07584e446486b48d16574380835f8b789
ntchris <ntchris@hotmail.com>
2016-12-27 09:22:20 -0500

    first commit after IAR destroyed all my source files when Windows blue screen
    first commit after IAR destroyed all my source files when Windows blue
    screen

---
+++ stm8_digitubeADC/source/stm8_DigitubeDriver.cpp
@@ -0,0 +1,18 @@
+
+
+
+#include "stm8_DigitubeDriver.hpp"
+
+
+void STM8_DigitubeDriver::tim4_int_init(void)
+{
+
+
+}
+
+
+void STM8_DigitubeDriverdigitubeInit(void)
+{
+
+
+}
---
+++ stm8_digitubeADC/source/stm8_DigitubeDriver.hpp
@@ -0,0 +1,20 @@
+
+#include "stm8s.h"
+
+// Connect a 4 digit digitube
+static const  uint8_t  MAX_DIGIT=4;
+
+class STM8_DigitubeDriver
+{
+
+  static void tim4_int_init(void);
+
+public:
+
+   static void digitubeInit(void);
+
+
+
+
+};
+
