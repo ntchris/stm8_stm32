2c37db344af4cff274462b77cfd088bdfe851a98
ntchris <ntchris@hotmail.com>
2016-12-27 23:43:13 -0500

    array read

--- stm8_digitubeADC/source/stm8_DigitubeDriver.cpp
+++ stm8_digitubeADC/source/stm8_DigitubeDriver.cpp
@@ -4,8 +4,33 @@
 
 #include "stm8_DigitubeDriver.hpp"
 
+
+//STM8_DigitubeDriver::Port_Pin STM8_DigitubeDriver::Segment_A;
+
+const STM8_DigitubeDriver::Port_Pin STM8_DigitubeDriver::Segment_A ={Segment_A_Port, Segment_A_Pin };
+const STM8_DigitubeDriver::Port_Pin STM8_DigitubeDriver::Segment_B ={Segment_B_Port, Segment_B_Pin };
+const STM8_DigitubeDriver::Port_Pin STM8_DigitubeDriver::Segment_C ={Segment_C_Port, Segment_C_Pin };
+const STM8_DigitubeDriver::Port_Pin STM8_DigitubeDriver::Segment_D ={Segment_D_Port, Segment_D_Pin };
+const STM8_DigitubeDriver::Port_Pin STM8_DigitubeDriver::Segment_E ={Segment_E_Port, Segment_E_Pin };
+const STM8_DigitubeDriver::Port_Pin STM8_DigitubeDriver::Segment_F ={Segment_F_Port, Segment_F_Pin };
+const STM8_DigitubeDriver::Port_Pin STM8_DigitubeDriver::Segment_G ={Segment_G_Port, Segment_G_Pin };
+const STM8_DigitubeDriver::Port_Pin STM8_DigitubeDriver::Segment_Dp ={Segment_Dp_Port, Segment_Dp_Pin };
+
+const STM8_DigitubeDriver::Port_Pin STM8_DigitubeDriver::Digit1 ={Segment_Digit1_Port, Segment_Digit1_Pin };
+const STM8_DigitubeDriver::Port_Pin STM8_DigitubeDriver::Digit2 ={Segment_Digit2_Port, Segment_Digit2_Pin };
+const STM8_DigitubeDriver::Port_Pin STM8_DigitubeDriver::Digit3 ={Segment_Digit3_Port, Segment_Digit3_Pin };
+const STM8_DigitubeDriver::Port_Pin STM8_DigitubeDriver::Digit4 ={Segment_Digit4_Port, Segment_Digit4_Pin };
+
+
+//STM8_DigitubeDriver::Port_Pin ArrayPortPin[MAX_Segment]={Segment_A, Segment_A, Segment_A, Segment_A, Segment_A, Segment_A, Segment_A    } ;
+
+
 void STM8_DigitubeDriver::gpioInitPushPull(GPIO_TypeDef* GPIOx, GPIO_Pin_TypeDef GPIO_Pin  )
 {
+
+
+
+
    GPIOx->CR2 &= (uint8_t)(~(GPIO_Pin));
    GPIOx->DDR |= (uint8_t)GPIO_Pin;  // Output mode
    GPIOx->CR1 |= (uint8_t)GPIO_Pin;  //push pull
@@ -30,8 +55,12 @@ void STM8_DigitubeDriver::stm8_Gpio_Write_High(GPIO_TypeDef* GPIOx, GPIO_Pin_Typ
 }
 
 
+
+
 void STM8_DigitubeDriver::stm8_Pins_For_DigitubeInit(void)
 {
+
+
    STM8_DigitubeDriver::gpioInitPushPull(Segment_A_Port,   Segment_A_Pin );
    STM8_DigitubeDriver::gpioInitPushPull(Segment_B_Port,   Segment_B_Pin );
    STM8_DigitubeDriver::gpioInitPushPull(Segment_C_Port,   Segment_C_Pin );
--- stm8_digitubeADC/source/stm8_DigitubeDriver.hpp
+++ stm8_digitubeADC/source/stm8_DigitubeDriver.hpp
@@ -1,6 +1,4 @@
 
-
-
 /**
  *  Work with Common Cathode 7 Seg digitube
  *
@@ -24,23 +22,23 @@
  */
 #include "stm8s.h"
 
-
-
-
 typedef enum
 {
-  GPIO_PIN_0    = ((uint8_t)0x01),  /*!< Pin 0 selected */
-  GPIO_PIN_1    = ((uint8_t)0x02),  /*!< Pin 1 selected */
-  GPIO_PIN_2    = ((uint8_t)0x04),  /*!< Pin 2 selected */
-  GPIO_PIN_3    = ((uint8_t)0x08),   /*!< Pin 3 selected */
-  GPIO_PIN_4    = ((uint8_t)0x10),  /*!< Pin 4 selected */
-  GPIO_PIN_5    = ((uint8_t)0x20),  /*!< Pin 5 selected */
-  GPIO_PIN_6    = ((uint8_t)0x40),  /*!< Pin 6 selected */
-  GPIO_PIN_7    = ((uint8_t)0x80),  /*!< Pin 7 selected */
-  GPIO_PIN_LNIB = ((uint8_t)0x0F),  /*!< Low nibble pins selected */
-  GPIO_PIN_HNIB = ((uint8_t)0xF0),  /*!< High nibble pins selected */
-  GPIO_PIN_ALL  = ((uint8_t)0xFF)   /*!< All pins selected */
-}GPIO_Pin_TypeDef;
+   GPIO_PIN_0 = ((uint8_t) 0x01), /*!< Pin 0 selected */
+   GPIO_PIN_1 = ((uint8_t) 0x02), /*!< Pin 1 selected */
+   GPIO_PIN_2 = ((uint8_t) 0x04), /*!< Pin 2 selected */
+   GPIO_PIN_3 = ((uint8_t) 0x08), /*!< Pin 3 selected */
+   GPIO_PIN_4 = ((uint8_t) 0x10), /*!< Pin 4 selected */
+   GPIO_PIN_5 = ((uint8_t) 0x20), /*!< Pin 5 selected */
+   GPIO_PIN_6 = ((uint8_t) 0x40), /*!< Pin 6 selected */
+   GPIO_PIN_7 = ((uint8_t) 0x80), /*!< Pin 7 selected */
+   GPIO_PIN_LNIB = ((uint8_t) 0x0F), /*!< Low nibble pins selected */
+   GPIO_PIN_HNIB = ((uint8_t) 0xF0), /*!< High nibble pins selected */
+   GPIO_PIN_ALL = ((uint8_t) 0xFF) /*!< All pins selected */
+} GPIO_Pin_TypeDef;
+
+
+
 
 
 
@@ -82,44 +80,70 @@ typedef enum
 
 
 
+
+
+
+
+
+
 typedef enum
 {
-  GPIO_MODE_IN_FL_NO_IT      = (uint8_t)0x00,  /*!< Input floating, no external interrupt */
-  GPIO_MODE_IN_PU_NO_IT      = (uint8_t)0x40,  /*!< Input pull-up, no external interrupt */
-  GPIO_MODE_IN_FL_IT         = (uint8_t)0x20,  /*!< Input floating, external interrupt */
-  GPIO_MODE_IN_PU_IT         = (uint8_t)0x60,  /*!< Input pull-up, external interrupt */
-  GPIO_MODE_OUT_OD_LOW_FAST  = (uint8_t)0xA0,  /*!< Output open-drain, low level, 10MHz */
-  GPIO_MODE_OUT_PP_LOW_FAST  = (uint8_t)0xE0,  /*!< Output push-pull, low level, 10MHz */
-  GPIO_MODE_OUT_OD_LOW_SLOW  = (uint8_t)0x80,  /*!< Output open-drain, low level, 2MHz */
-  GPIO_MODE_OUT_PP_LOW_SLOW  = (uint8_t)0xC0,  /*!< Output push-pull, low level, 2MHz */
-  GPIO_MODE_OUT_OD_HIZ_FAST  = (uint8_t)0xB0,  /*!< Output open-drain, high-impedance level,10MHz */
-  GPIO_MODE_OUT_PP_HIGH_FAST = (uint8_t)0xF0,  /*!< Output push-pull, high level, 10MHz */
-  GPIO_MODE_OUT_OD_HIZ_SLOW  = (uint8_t)0x90,  /*!< Output open-drain, high-impedance level, 2MHz */
-  GPIO_MODE_OUT_PP_HIGH_SLOW = (uint8_t)0xD0   /*!< Output push-pull, high level, 2MHz */
-}GPIO_Mode_TypeDef;
+   GPIO_MODE_IN_FL_NO_IT = (uint8_t) 0x00, /*!< Input floating, no external interrupt */
+   GPIO_MODE_IN_PU_NO_IT = (uint8_t) 0x40, /*!< Input pull-up, no external interrupt */
+   GPIO_MODE_IN_FL_IT = (uint8_t) 0x20, /*!< Input floating, external interrupt */
+   GPIO_MODE_IN_PU_IT = (uint8_t) 0x60, /*!< Input pull-up, external interrupt */
+   GPIO_MODE_OUT_OD_LOW_FAST = (uint8_t) 0xA0, /*!< Output open-drain, low level, 10MHz */
+   GPIO_MODE_OUT_PP_LOW_FAST = (uint8_t) 0xE0, /*!< Output push-pull, low level, 10MHz */
+   GPIO_MODE_OUT_OD_LOW_SLOW = (uint8_t) 0x80, /*!< Output open-drain, low level, 2MHz */
+   GPIO_MODE_OUT_PP_LOW_SLOW = (uint8_t) 0xC0, /*!< Output push-pull, low level, 2MHz */
+   GPIO_MODE_OUT_OD_HIZ_FAST = (uint8_t) 0xB0, /*!< Output open-drain, high-impedance level,10MHz */
+   GPIO_MODE_OUT_PP_HIGH_FAST = (uint8_t) 0xF0, /*!< Output push-pull, high level, 10MHz */
+   GPIO_MODE_OUT_OD_HIZ_SLOW = (uint8_t) 0x90, /*!< Output open-drain, high-impedance level, 2MHz */
+   GPIO_MODE_OUT_PP_HIGH_SLOW = (uint8_t) 0xD0 /*!< Output push-pull, high level, 2MHz */
+} GPIO_Mode_TypeDef;
 
 
 
 // Connect a 4 digit digitube
-static const  uint8_t  MAX_DIGIT=4;
+static const uint8_t MAX_DIGIT = 4;
+static const uint8_t MAX_Segment = 7;  // A - G  , Dp doesn't count
+
+
+
+
+
+
+
 
 class STM8_DigitubeDriver
 {
 
-  static void tim4_Interupt_Init(void);
+   struct Port_Pin
+   {
+      GPIO_TypeDef *port;
+      GPIO_Pin_TypeDef pin;
+   };
+
+   static const  Port_Pin Segment_A, Segment_B, Segment_C, Segment_D, Segment_E, Segment_F, Segment_G, Segment_Dp;
+   static const  Port_Pin Digit1, Digit2, Digit3, Digit4;
+
+   const static  Port_Pin ArrayPortPin[MAX_Segment];
+   //={Segment_A, Segment_A, Segment_A, Segment_A, Segment_A, Segment_A, Segment_A    } ;
 
 
-  static void  gpioInitPushPull(GPIO_TypeDef* GPIOx, GPIO_Pin_TypeDef GPIO_Pin );
+   static void tim4_Interupt_Init(void);
+
+   static void gpioInitPushPull(GPIO_TypeDef* GPIOx, GPIO_Pin_TypeDef GPIO_Pin);
+
+   static void displaySegA();
 
 
 public:
 
    static void stm8_Pins_For_DigitubeInit(void);
 
-
    static void stm8_Gpio_Write_Low(GPIO_TypeDef* GPIOx, GPIO_Pin_TypeDef PortPins);
-    static void stm8_Gpio_Write_High(GPIO_TypeDef* GPIOx, GPIO_Pin_TypeDef PortPins);
-
+   static void stm8_Gpio_Write_High(GPIO_TypeDef* GPIOx, GPIO_Pin_TypeDef PortPins);
 
 };
 
