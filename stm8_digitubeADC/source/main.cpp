138eab0c6ff59e0295c0780eb8b3d1538e346d8e
ntchris <ntchris@hotmail.com>
2017-01-08 14:55:59 -0500

    continue work on pcb

--- stm8_digitubeADC/source/main.cpp
+++ stm8_digitubeADC/source/main.cpp
@@ -1,48 +1,59 @@
 #include "stm8_DigitubeDriver.hpp"
 
-#define TESTBUILD
+//#define TESTBUILD
+/*
+ #ifdef TESTBUILD
 
-#ifdef TESTBUILD
-void doTest()
-{
 
-   STM8_DigitubeDriver::displayString("12.34");
+ void doTest()
+ {
 
-   STM8_DigitubeDriver::displayString("13..4");
-   STM8_DigitubeDriver::displayString("....");
+ STM8_DigitubeDriver::displayString("12.34");
 
-   STM8_DigitubeDriver::displayString("...1");
-   STM8_DigitubeDriver::displayString("1..2");
+ STM8_DigitubeDriver::displayString("13..4");
+ STM8_DigitubeDriver::displayString("....");
 
-   STM8_DigitubeDriver::displayInt(1234);
-   STM8_DigitubeDriver::displayInt(5);
-   STM8_DigitubeDriver::displayInt(12);
+ STM8_DigitubeDriver::displayString("...1");
+ STM8_DigitubeDriver::displayString("1..2");
 
-   STM8_DigitubeDriver::displayFloat(5.123); // only show 5.12
-   STM8_DigitubeDriver::displayFloat(12.34); //
-   STM8_DigitubeDriver::displayVoltage(9.8765);
-   STM8_DigitubeDriver::displayInt(0);
+ STM8_DigitubeDriver::displayInt(1234);
+ STM8_DigitubeDriver::displayInt(5);
+ STM8_DigitubeDriver::displayInt(12);
 
-}
+ STM8_DigitubeDriver::displayFloat(5.123); // only show 5.12
+ STM8_DigitubeDriver::displayFloat(12.34); //
+ STM8_DigitubeDriver::displayVoltage(9.8765);
+ STM8_DigitubeDriver::displayInt(0);
 
-#endif
+ }
+
+ #endif
+
+ */
 
 void Delayms(unsigned int ms);
 
 int main()
 {
+   int loopIntervalms = 600;
+   int MaxHaltTimeSecond = 25 * 1000 / loopIntervalms; // 50*600=30sec sleep
 
    STM8_DigitubeDriver::stm8_init();
+   Delayms(loopIntervalms);
 
-
+   int haltCounter = 0;
    while (true)
    {
+      haltCounter++;
+      if (haltCounter >= MaxHaltTimeSecond)
+      {
+         haltCounter = 0;
+         halt();
+      }
       //STM8_DigitubeDriver::displayFloat(1.234); // missing seg G
 
-      //ADC1->CR1 |= ADC1_CR1_ADON;
-      //STM8_DigitubeDriver::setDisplayBufferOverflow();
-      Delayms(600);
       STM8_DigitubeDriver::startADC();
+      Delayms(loopIntervalms);
 
    };
 
